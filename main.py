84835c357fee739cc4c8335933bb3f882b8914be
Thiago Monteiro <tsmonteiro@gmail.com>
2023-10-30 20:03:35 +0000

    log

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ ctx = context.TercenContext()
 if not ctx.task is None:
     envDict = ctx.task.environment
     for e in envDict:
+        ctx.log(e)
         if isinstance(e, dict):
             for key, value in e.items():
                 if key == "task.siblings.id":
