78e501ecdb0fd5c33d638bb6b1c39e503f0b6c83
Thiago <tsmonteiro@gmail.com>
2023-11-09 13:52:02 +0000

    arr to str

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ if not ctx.task is None:
             ctx.log(str(e.key))
             if str(e.key) == "task.siblings.id":
                 ctx.log("Found task sibling")
-                ctx.log(str(e.value))
-                ctx2 = context.TercenContext(taskId=e.value)
+                ctx.log(str(e.value[0]))
+                ctx2 = context.TercenContext(taskId=e.value[0])
 
                 if ctx2 is None:
                     ctx.log("Failed to create context 2")
