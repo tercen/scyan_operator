8f3de48b55b3f8752bc6181f3fa61ebd8f423fb8
Thiago <tsmonteiro@gmail.com>
2023-11-09 13:18:44 +0000

    dbg

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ if not ctx.task is None:
         if isinstance(e, Pair):
             ctx.log(str(e.key))
             if str(e.key) == "task.siblings.id":
+                ctx.log("Found task sibling")
+                ctx.log(str(e.value))
                 ctx2 = context.TercenContext(taskId=e.value)
 
                 if ctx2 is None:
@@ -35,8 +37,9 @@ if not ctx.task is None:
  
                 #docker run --rm --init --memory 1124m --memory-swap 1124m --memory-swappiness 0 --name=tercen_task_a9a0da8626f5c8b833542660f200a193 --env TERCEN_SERVICE_URI=http://172.42.0.42:5400 --env TERCEN_WEB_APP_PORT=8080 tercen/scyan_operator:latest --taskId a9a0da8626f5c8b833542660f200a193 --serviceUri http://172.42.0.42:5400 --token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3RlcmNlbi5jb20iLCJleHAiOjE2OTkxMTE2ODcsImRhdGEiOnsiZCI6IiIsInUiOiJ0ZXN0IiwiZSI6MTY5OTExMTY4NzA4NH19.xvySAwvnrv-Sg9LY6lH0vwYA8tPBPNHLQOuuU8MQMP0[0m
 else:
-    
-    ctx2 = context.TercenContext(workflowId="e2a9ef1dc04be286be60a5082d013ce8", stepId="5d51ba5d-8fba-4978-9fc1-3b5d2ccbe995")
+    # Raise error
+    ctx2 = None
+    # ctx2 = context.TercenContext(workflowId="e2a9ef1dc04be286be60a5082d013ce8", stepId="5d51ba5d-8fba-4978-9fc1-3b5d2ccbe995")
 
 
 
