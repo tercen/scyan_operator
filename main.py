dea271569507a4b11ed3a3abdba5c0e8330ebbe7
Thiago Monteiro <tsmonteiro@gmail.com>
2024-01-31 16:55:36 +0000

    mem improvement for hierarchy output

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ from tercen.model.impl import Pair
 import numpy as np
 import polars as pl
 import pandas as pd
-import scyan, anndata, os
+import scyan, anndata
 import matplotlib
 matplotlib.use('Agg')
 
-#http://127.0.0.1:5400/test/w/5409bc1875748e715c48848fd3004e42/ds/1ba15732-ba1e-400c-828d-8e542decfc5c
-#http://127.0.0.1:5400/test/w/5409bc1875748e715c48848fd3004e42/ds/9deae2f2-b062-46bd-a36b-8d5c445e3d4b
-# ctx = context.TercenContext(workflowId="5409bc1875748e715c48848fd3004e42",\
-                            #  stepId="1ba15732-ba1e-400c-828d-8e542decfc5c")
+from memory_profiler import profile
 
-ctx = context.TercenContext()
+def tercenBool(x):
+    return x == 'true'
+
+
+ctx = context.TercenContext(workflowId="5409bc1875748e715c48848fd3004e42",\
+                            stepId="1ba15732-ba1e-400c-828d-8e542decfc5c")
+
+# ctx = context.TercenContext()
 
 if not ctx.task is None:
     envPairs = ctx.task.environment
@@ -23,7 +27,7 @@ if not ctx.task is None:
             if str(e.key) == "task.siblings.id":                
                 nChar = len(e.value)
                 taskId = e.value[2:(nChar-2)]
-  
+
                 ctx2 = context.TercenContext(taskId=taskId)
 
                 if ctx2 is None:
@@ -31,21 +35,38 @@ if not ctx.task is None:
 
 else:
     ctx2 = None
-    # ctx2 = context.TercenContext(workflowId="5409bc1875748e715c48848fd3004e42",\
-                                #   stepId="9deae2f2-b062-46bd-a36b-8d5c445e3d4b")
 
+
+fullOutput = ctx.operator_property('FullOutput', typeFn=tercenBool, default=False)
+priorSd = ctx.operator_property('PriorSD', typeFn=float, default=0.3)
+lr = ctx.operator_property('LR', typeFn=float, default=0.0005)
+nLayers = ctx.operator_property('Layers', typeFn=int, default=7)
+nHiddenLayers = ctx.operator_property('Hidden Layers', typeFn=int, default=6)
+hiddenSz = ctx.operator_property('Hidden Size', typeFn=int, default=16)
+temperature = ctx.operator_property('Temperature', typeFn=float, default=0.5)
+moduloTemp = ctx.operator_property('Modulo Temp', typeFn=int, default=3)
+batchSize = ctx.operator_property('Batch Size', typeFn=int, default=8192)
+warmUp = ctx.operator_property('WarmUp', typeFn=str, default="(0.35,4)")
+w1 = float(warmUp.split(",")[0].replace("(", "").strip())
+w2 = float(warmUp.split(",")[1].replace(")", "").strip())
 
 
 yDf = ctx.select([".y", ".ci", ".ri"])
 colDf = ctx.cselect([""])
-colDf = colDf.select('obs_' + pl.col(colDf.columns[0]).cast(pl.Utf8))
+# colDf = colDf.select(pl.col(colDf.columns[0]).cast(pl.Int32).apply(hex) )
+
+colDf = colDf.select('o' + pl.col(colDf.columns[0]).cast(pl.Int32).cast(pl.Utf8))
 colDf = colDf.with_columns(pl.Series(name=".ci", values=range(0,len(colDf) ), dtype=pl.Int32) )
+
 
 rowDf = ctx.rselect([""])
 rowDf = rowDf.with_columns(pl.Series(name=".ri", values=range(0,len(rowDf)), dtype=pl.Int32))
 
-yDf = yDf.join(colDf, on=".ci").join(rowDf, ".ri")
-yDf = yDf.drop([".ri", ".ci"])
+yDf = yDf.join(colDf, on=".ci")\
+            .drop(".ci")\
+            .join(rowDf, ".ri")\
+            .drop(".ri")
+
 
 annDf = ctx2.select([".y", ".ci", ".ri"])
 
@@ -68,8 +89,7 @@ yDfP = yDf.pivot(columns=yDf.columns[2], index=yDf.columns[1], values=yDf.column
 
 
 markers = np.intersect1d(yDfP.columns[1:], annDfP.columns[len(ctx2.rnames):])
-population = annDfP[:,0].to_numpy()
-
+# population = annDfP[:,0].to_numpy()
 
 adata = anndata.AnnData(  yDfP.to_numpy()[:,1:].astype(np.float32) )
 
@@ -89,23 +109,10 @@ for i in range(0, len(ctx2.rnames)):
     else:
         tablePd[ctx2.rnames[i]] = annDfP[ctx2.rnames[i]].to_numpy() 
 
-tablePd[ctx2.rnames] = tablePd[ctx2.rnames].astype('category')#.cat.codes.unstack()
-tablePd = tablePd.set_index(ctx2.rnames)
-def tercenBool(x):
-    return x == 'true'
-fullOutput = ctx.operator_property('FullOutput', typeFn=tercenBool, default=False)
 
-priorSd = ctx.operator_property('PriorSD', typeFn=float, default=0.3)
-lr = ctx.operator_property('LR', typeFn=float, default=0.0005)
-nLayers = ctx.operator_property('Layers', typeFn=int, default=7)
-nHiddenLayers = ctx.operator_property('Hidden Layers', typeFn=int, default=6)
-hiddenSz = ctx.operator_property('Hidden Size', typeFn=int, default=16)
-temperature = ctx.operator_property('Temperature', typeFn=float, default=0.5)
-moduloTemp = ctx.operator_property('Modulo Temp', typeFn=int, default=3)
-batchSize = ctx.operator_property('Batch Size', typeFn=int, default=8192)
-warmUp = ctx.operator_property('WarmUp', typeFn=str, default="(0.35,4)")
-w1 = float(warmUp.split(",")[0].replace("(", "").strip())
-w2 = float(warmUp.split(",")[1].replace(")", "").strip())
+tablePd[ctx2.rnames] = tablePd[ctx2.rnames].astype('category')
+tablePd = tablePd.set_index(ctx2.rnames)
+
 
 model = scyan.Scyan(adata=adata, table=tablePd, \
                     prior_std=priorSd, lr=lr, n_layers=nLayers, \
@@ -116,91 +123,62 @@ model = scyan.Scyan(adata=adata, table=tablePd, \
 
 
 ctx.log("Fitting model...")
-model.fit( )
+model.fit(  )
 
 
 ctx.log("Predicting cell populations...")
 model.predict()
 
-
 outDf = None
-dfList = [None] * len(adata.obs_names) 
-if fullOutput == True:
-    dfList2 = [None] * (len(adata.obs_names)*len(population)) # 2 -> levels
 
-
-popLabels = [""]
-[popLabels.append(l) for l in model.level_names]
-
-
-logProbsList = []
-outColNames = []
-tercenColNames = []
 ctx.log("Calculating population probabilities")
-for i in range(0, len(popLabels)):
-    popLabel = popLabels[i]
-    tercenColName = ctx2.rnames[i]
+outDf = None
+for j in range(0, len(model.level_names)):
+    popLabel = model.level_names[j]
+    tercenColName = ctx2.rnames[j]
     if popLabel == "":
         outColName = "scyan_pop"
     else:
         outColName = "scyan_pop_{}".format(popLabel)
 
-    outColNames.append(outColName)
-    tercenColNames.append(tercenColName)
     fakeSeries = model.adata.obs[outColName] != 'NoPop'
     fakeSeries[0] = True
-    u = model(fakeSeries)
+    logProbs = model.module.prior.log_prob(model(fakeSeries))
 
-    logProbsList.append( model.module.prior.log_prob(u))
+    dfList = [] 
+    # if fullOutput == True:
+    #     dfList2 = [] # * (len(adata.obs_names)*len(population)) # 2 -> levels
 
+    predColName = "PredictedPopulation_{}".format(tercenColName)
+    predMaxLogProbName  = "MaxLogProb_{}".format(tercenColName)
 
-ctx.log("Creating output tables")
+    for i in range(0, len(model.adata.obs_names)):
+        tmpDf = pl.DataFrame({".ci":int(i) })
 
-idx = -1
-    
-for i in range(0, len(adata.obs_names)):
-    tmpDf = pl.DataFrame({".ci":int(i) })
-
-    for j in range(0, len(popLabels)):
-        outColName = outColNames[j]
-        tercenColName = tercenColNames[j]
-        logProbs = logProbsList[j]
         if isinstance( model.adata.obs[outColName].iloc[i], str ):
             pop = model.adata.obs[outColName].iloc[i] 
         else:
             pop = "None"
 
-        tmpDf = tmpDf.with_columns(pl.lit(pop).alias("PredictedPopulation_{}".format(tercenColName) ) ).\
-                      with_columns(pl.lit(np.max(logProbs[i,:].tolist())).alias("MaxLogProb_{}".format(tercenColName) ) )
-    
-        if fullOutput:
-            tmpDf2 = pl.DataFrame({".ci":int(i)})
-            logPops = logProbs[i,:].tolist()
-            probs = (np.exp(logPops) / (np.exp(logPops)).sum()).tolist()
-            for p in population:
-                                # tmpDf2 = pl.DataFrame({".ci":int(i), "Population":p, \
-                                #     "LogProb":logPops.pop(0), \
-                                #         "Prob":probs.pop(0),\
-                                #         "Level":tercenColName})
-                tmpDf2 = pl.DataFrame({".ci":int(i)})
-                tmpDf2.with_columns(pl.lit(logPops.pop(0)).alias("LogProb_{}".format(tercenColName))).\
-                    with_columns(pl.lit(probs.pop(0)).alias("Prob_{}".format(tercenColName))).\
-                    with_columns(pl.lit(p).alias("Population_{}".format(tercenColName)))
-
-
-                if j == 0:
-                    idx = idx + 1
-                dfList2[idx] = tmpDf2
-                
+        tmpDf = tmpDf.with_columns(pl.lit(pop).alias(predColName ) ).\
+                    with_columns(pl.lit(np.max(logProbs[i,:].tolist())).alias(predMaxLogProbName ) )
         
-    dfList[i] = tmpDf
-    
 
+        dfList.append(tmpDf)
+
+    if outDf is None:
+        outDf = pl.concat(dfList)
+    else:
+        outDf = outDf.join(  pl.concat(dfList), on=".ci" ) 
+    dfList = None
+
+    
+model = None
 
 ctx.log("Saving outDf")
 
-outDf =  pl.concat(dfList)
-dfList.clear()
+# outDf =  pl.concat(dfList)
+# dfList.clear()
 outDf = outDf.with_columns(pl.col(".ci").cast(pl.Int32))
 outDf = ctx.add_namespace(outDf) 
 
@@ -213,3 +191,5 @@ if fullOutput:
 
 else:
     ctx.save([outDf])
+
+
